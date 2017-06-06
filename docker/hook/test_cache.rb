5618707011fbd36bd6a9a601e045180a637e0630
Morioka Shuhei <selmertsx@gmail.com>
2017-06-07 00:06:09 +0900

    mysql settings

--- docker/hook/test_cache.rb
+++ docker/hook/test_cache.rb
@@ -1,3 +1,10 @@
 cache = Cache.new(namespace: 'sample', size_mb: 2)
 cache["hoge"] = "hage"
+
+db = MySQL::Database.new('db', 'root', '', 'information_schema')
+
+db.execute('select * from TABLES') do |row, fields|
+  Nginx.echo row
+end
+
 Nginx.echo cache["hoge"]
