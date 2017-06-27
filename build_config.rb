f779ba88f920be49432cb5e3d66209e5d89620e5
Morioka Shuhei <selmertsx@gmail.com>
2017-06-27 10:18:43 +0900

    fix build_config

--- build_config.rb
+++ build_config.rb
@@ -1,16 +1,10 @@
 MRuby::Build.new('host') do |conf|
-
   toolchain :gcc
-
   conf.gembox 'full-core'
-
   conf.cc do |cc|
     cc.flags << ENV['NGX_MRUBY_CFLAGS'] if ENV['NGX_MRUBY_CFLAGS']
   end
-
-  #
-  # Recommended for ngx_mruby
-  #
+  # mrbgems from github
   conf.gem :github => 'iij/mruby-io'
   conf.gem :github => 'iij/mruby-env'
   conf.gem :github => 'iij/mruby-dir'
@@ -27,45 +21,24 @@ MRuby::Build.new('host') do |conf|
   conf.gem :github => 'matsumotory/mruby-uname'
   conf.gem :github => 'matsumotory/mruby-mutex'
   conf.gem :github => 'matsumotory/mruby-localmemcache'
+  conf.gem :github => 'mattn/mruby-mysql'
+
+  # mrbgems from mgem
   conf.gem :mgem => 'mruby-secure-random'
 
   # ngx_mruby extended class
   conf.gem './mrbgems/ngx_mruby_mrblib'
   conf.gem './mrbgems/rack-based-api'
   conf.gem './mrbgems/auto-ssl'
-
-  # use memcached
-  # conf.gem :github => 'matsumotory/mruby-memcached'
-
-  # build error on travis ci 2014/12/01, commented out mruby-file-stat
-  # conf.gem :github => 'ksss/mruby-file-stat'
-
-  # use markdown on ngx_mruby
-  # conf.gem :github => 'matsumotory/mruby-discount'
-
-  # use mysql on ngx_mruby
-  conf.gem :github => 'mattn/mruby-mysql'
-  # have GeoIPCity.dat
-  # conf.gem :github => 'matsumotory/mruby-geoip'
-
-  # Linux only for ngx_mruby
-  # conf.gem :github => 'matsumotory/mruby-capability'
-  # conf.gem :github => 'matsumotory/mruby-cgroup'
-
 end
 
 MRuby::Build.new('test') do |conf|
-  # load specific toolchain settings
-
-  # Gets set by the VS command prompts.
   if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
     toolchain :visualcpp
   else
     toolchain :gcc
   end
-
   enable_debug
-
   conf.gem :github => 'matsumotory/mruby-simplehttp'
   conf.gem :github => 'matsumotory/mruby-httprequest'
   conf.gem :github => 'matsumotory/mruby-uname'
@@ -76,7 +49,5 @@ MRuby::Build.new('test') do |conf|
   conf.gem :github => 'iij/mruby-socket'
   conf.gem :github => 'iij/mruby-pack'
   conf.gem :github => 'iij/mruby-env'
-
-  # include the default GEMs
   conf.gembox 'full-core'
 end
